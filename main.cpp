e94cc8d47d280400a138b8d04f463ca0f9116796
Justin Schwerdtfeger <justinschwerdtfeger245@gmail.com>
2023-11-13 13:15:00 -0600

    created basic function templates

--- main.cpp
+++ main.cpp
@@ -1,4 +1,44 @@
 #include <iostream>
-void intro();
+#include <string>
+using namespace std;
 
-int main() { return 0; }
+void intro();
+string getWord(int lenghtOfWord);
+int getNumberOfGuesses();
+int getLengthOfWord();
+void game(int numberOfGuesses, string word);
+
+int main() {
+    intro();
+    int numberOfGuesses = getNumberOfGuesses();
+    int lengthOfWord = getLengthOfWord();
+    string word = getWord(lengthOfWord);
+    game(numberOfGuesses, word);
+    return 0;
+}
+
+// Justin
+void intro() {}
+
+// Luke
+// This should read the lengthOf word and read the corresponding file (there is
+// only 1 so far which is for 5 letter words)
+string getWord(int lengthOfWord) {
+
+    // For now only 5 letter words should be allowe
+    return "";
+}
+
+// Dylan - This function should ask the user how many guesses they would like
+// and return that number. It needs to have input validation
+int getNumberOfGuesses() {}
+
+// Dylan - This function should ask the user for the length of the word. Be sure
+// to use input validation and if the user enters an invalid number meaning if
+// they type anything other than 5, they will be prompted again
+int getLengthOfWord() {
+    // For now only 5 letter words should be allowed
+    // If the user enters any length other than 5 it should not work
+}
+
+void game(int numberOfGuesses, string word) {}
