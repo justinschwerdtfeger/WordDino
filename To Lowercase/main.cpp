03919117c095a53cd4103b0e426d6fe20e771a1a
Justin <justinschwerdtfeger245@gmail.com>
2023-11-25 20:15:30 -0600

    create to lowercase function

---
+++ To Lowercase/main.cpp
@@ -0,0 +1,30 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+string toLower(const string &input) {
+    string output;
+    for (char character : input) {
+        output += tolower(character);
+    }
+    return output;
+}
+int main() {
+    ifstream inFile;
+    ofstream outFile;
+    vector<string> words;
+    string contents;
+
+    inFile.open("word-list-6-letter.txt");
+    outFile.open("word-list-6-letter-corrected.txt");
+
+    while (inFile >> contents) {
+        contents = toLower(contents);
+        words.push_back(contents);
+        cout << contents << endl;
+    }
+
+    inFile.close();
+    return 0;
+}
